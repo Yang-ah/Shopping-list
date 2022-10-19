b718aba9490750b1a0949944efeaa69b0770c0be
Yang-ah <yangah0409@gmail.com>
2022-10-19 20:15:06 +0900

    2) main 구축하기

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-// 1. loadItems : data를 받아오면, promise를 return하는 함수
-// items를 그냥 return하지 않고 promise를 return하는 이유 : data.json을 동적으로 읽어오는 시간이 있기 때문
-// then(성공), items을 받아와서 함수를 실행
-// catch(실패), 콘솔에 나타냄.
+// 2. displayItems : data를 넘겨받아 HTML에 보여주는 함수
+// items를 성공적으로 받아온 뒤,
+// 받아온 items들을 displayItems 함수를 통해
+// HTML에 보여주가
 
 loadItems()
-  .then((items) => {})
+  .then((items) => {
+    displayItems(items);
+  })
   .catch(console.log);
