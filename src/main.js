25a67deb22a93ca34393b956f98953cef775ffbd
Yang-ah <yangah0409@gmail.com>
2022-10-20 17:08:39 +0900

    5) display items
    added functions (displayItems, createHTMLString)

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
 // Fetch the items from the JSON file
 function loadItems() {
-  return (
-    fetch("./data/data.json")
-      // .then((response) => console.log(response))
-      .then((response) => response.json())
-      // .then((response) => console.log(json))
-      .then((json) => json.items)
-  );
+  return fetch("./data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+// Create HTML list item form the given data item
+function createHTMLString(item) {
+  return `<li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+    <span class="item_description">${item.gender}, ${item.size}</span>
+  </li>`;
 }
 
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);
