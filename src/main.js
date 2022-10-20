70eaa471d0762090323cd7adcd962b18eebe27dd
Yang-ah <yangah0409@gmail.com>
2022-10-20 10:59:44 +0900

    4) fetch data
    added 'loadItems' function for fetching data.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-// 3. setEventListeners : 버튼을 누르면 필터링을 해주는 이벤트리스너
+// Fetch the items from the JSON file
+function loadItems() {
+  return (
+    fetch("./data/data.json")
+      // .then((response) => console.log(response))
+      .then((response) => response.json())
+      // .then((response) => console.log(json))
+      .then((json) => json.items)
+  );
+}
 
+// main
 loadItems()
   .then((items) => {
-    displayItems(items);
-    setEventListeners(items);
+    console.log(items);
+    // displayItems(items);
+    // setEventListeners(items);
   })
   .catch(console.log);
